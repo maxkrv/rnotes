5493ad2a641a916068c9c64b38572cc107838c19
max_krv <maxkr1203@gmail.com>
2022-08-24 13:08:42 +0300

    add layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import Layout from "../components/layout";
 
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThemeProvider enableSystem attribute="class">
-			<Component {...pageProps} />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
 		</ThemeProvider>
 	);
 }
