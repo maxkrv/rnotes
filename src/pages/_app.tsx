425954d6ae8c31e222827f6726984e05f141dbc1
max_krv <maxkr1203@gmail.com>
2022-09-01 16:50:19 +0300

    add providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,20 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import Layout from "../components/layout";
+import { SessionProvider } from "next-auth/react";
+import { StyledEngineProvider } from "@mui/material";
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps: { session, pageProps } }: AppProps) {
 	return (
-		<ThemeProvider enableSystem attribute="class">
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</ThemeProvider>
+		<SessionProvider session={session}>
+			<ThemeProvider enableSystem attribute="class">
+				<StyledEngineProvider injectFirst>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</StyledEngineProvider>
+			</ThemeProvider>
+		</SessionProvider>
 	);
 }
 
