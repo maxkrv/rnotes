d6f1cf5d8f8b155b1f6cb6074aa2dc359cf696c9
max_krv <maxkr1203@gmail.com>
2022-09-01 16:49:08 +0300

    update header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,8 @@ import React, { ComponentPropsWithoutRef, FC } from "react";
 import classes from "./header.module.scss";
 import Container from "../ui/container/container";
 import ThemeSwitcher from "../themeSwitcher/themeSwitcher";
+import Link from "next/link";
+import { Button } from "@mui/material";
 
 interface HeaderProps extends ComponentPropsWithoutRef<"header"> {}
 
@@ -10,8 +12,23 @@ const Header: FC<HeaderProps> = ({ ...props }) => {
 		<header className={classes.header} {...props}>
 			<Container>
 				<div className={classes.header__inner}>
-					<h1 className="logo">Rnotes</h1>
-					<ThemeSwitcher />
+					<h1 className="logo">
+						<Link href="/">
+							<a>Rnotes</a>
+						</Link>
+					</h1>
+					<div className={classes.header__buttons}>
+						<ThemeSwitcher />
+						<Link href="/login">
+							<Button
+								component="a"
+								variant="outlined"
+								className="text-white"
+							>
+								<a>Sign in</a>
+							</Button>
+						</Link>
+					</div>
 				</div>
 			</Container>
 		</header>
