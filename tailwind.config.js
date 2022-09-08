bf729d6719614204380db1408154e67b65ffde34
max_krv <maxkr1203@gmail.com>
2022-09-08 22:41:51 +0300

    remove vh fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,6 @@ module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
-		height: (theme) => ({
-			auto: "auto",
-			...theme("spacing"),
-			full: "100%",
-			screen: "calc(var(--vh) * 100)",
-		}),
-		minHeight: (theme) => ({
-			0: "0",
-			...theme("spacing"),
-			full: "100%",
-			screen: "calc(var(--vh) * 100)",
-		}),
 	},
 	darkMode: "class",
 	plugins: [require("tailwindcss-global-dark")],
